7df6ae51368b38c874ad20b7689bceb1d2ad7fcd
Samu163 <125267861+Samu163@users.noreply.github.com>
2024-01-09 09:15:00 +0100

    puch

--- Physics II - Bullet3D - Handout 05/ModuleSceneIntro.cpp
+++ Physics II - Bullet3D - Handout 05/ModuleSceneIntro.cpp
@@ -22,11 +22,13 @@ bool ModuleSceneIntro::Start()
 	App->camera->LookAt(vec3(0, 40, 0));
 	//{coordenadas de posicion (donde aparece)}, {Tamaño (x,y,z)}, Color, Angulos (x,y,z)
 	//El tamaño empieza en el 0,0, es decir, si es 100 va a ser 50 a un lado y 50 a otro
-	////Primera recta 
-	//AddCube({ 1, 30, 1 }, { 30, 0, 200 }, Grey, 0, 0, 0);
-	////Curva1 hacia la izquierda
-	//AddCube({ 4.5, 30, 111.5 }, { 30, 0, 30 }, Grey, 0, 15, 0);
-	////AddCube({ 10, 1, 00 }, { 2, 2, 10 }, Red, 0, 1, 0);
+	//Primera recta 
+	AddCube({ 537.9103,30, 297.5769 }, { 46.6435, 0, 166.0748 }, Grey, 0, 0, 0);
+	//Curva1 hacia la izquierda
+	AddCube({ 530.3091, 30, 207.8799 }, { 61.8459, 0, 57.8398 }, Grey, 0, 26.974, 0);
+	AddCube({ 509.8372, 30, 185.0201 }, { 72.1752, 0, 71.5963 }, Grey, 0, 56.122, 0);
+	AddCube({ 431.3337, 30, 170.0843 }, { 140.6841, 0, 44.5639 }, Grey, 0, 90, 0);
+	//AddCube({ 10, 1, 00 }, { 2, 2, 10 }, Red, 0, 1, 0);
 
 	return ret;
 }
